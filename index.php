98c273546d25a095d9444e5cc35d6be70378b93d
Tatiana Solovey <razhivina.t@mail.ru>
2024-12-19 22:59:47 +0300

    change commit

--- index.php
+++ index.php
@@ -1,38 +1,42 @@
 <?php
-if(isset($argv[1])){
-    $startDate = $argv[1];
+if(isset($argv[1]) && isset($argv[2])){
+    getSchedule($argv[1],$argv[2]);
 } else {
-    $startDate = 'first day of this month midnight';
+     getSchedule('','');
 }
-
-$dt = new DateTimeImmutable($startDate);
-$daysInMonth = cal_days_in_month(CAL_GREGORIAN, date('m'), date('Y'));
-
-$workingDays = [];
-$wDayCnt = 0;
-for ($day = 0; $day < $daysInMonth; ++$day) {
-    $d = $dt->add(new DateInterval('P' . $day . 'D'));
-    $workingDays[$day]['date']= $d->format('Y-m-d');
-    $workingDays[$day]['work'] = '';
-  
-    if ($wDayCnt % 3 === 0) {    
-           
-        if ($d->format('N') < 6) { 
-            
-            $workingDays[$day]['work']='+';  
-                  
-        } else { 
-            $wDayCnt = 0;
-            continue;
-        }
+function getSchedule($year, $month){
+    if($year != ''){
+        $startDate = $year.'-'.$month.'-01';
+    } else {
+        $startDate = 'first day of this month midnight';
     }
-        
-    ++$wDayCnt;
+    $dt = new DateTimeImmutable($startDate);
+    $daysInMonth = cal_days_in_month(CAL_GREGORIAN, date('m'), date('Y'));
+    
+    $workingDays = [];
+    $wDayCnt = 0;
+    for ($day = 0; $day < $daysInMonth; ++$day) {
+        $d = $dt->add(new DateInterval('P' . $day . 'D'));
+        $workingDays[$day]['date']= $d->format('Y-m-d');
+        $workingDays[$day]['work'] = '';
+      
+        if ($wDayCnt % 3 === 0) {    
+               
+            if ($d->format('N') < 6) { 
+                
+                $workingDays[$day]['work']='+';  
+                      
+            } else { 
+                $wDayCnt = 0;
+                continue;
+            }
+        }
+            
+        ++$wDayCnt;
+    }
+    echo "date\t\twork" . PHP_EOL;
+    foreach($workingDays as $day){    
+        echo $day['date'] . "\t" . $day['work'];
+        echo PHP_EOL;
+    }   
 }
-
-echo "date\t\twork" . PHP_EOL;
-foreach($workingDays as $day){    
-    echo $day['date'] . "\t" . $day['work'];
-    echo PHP_EOL;
-}
-
